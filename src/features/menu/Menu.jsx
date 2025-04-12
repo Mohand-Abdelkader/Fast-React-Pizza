d454865f26005879f88f407b14cd7b29d1ee7f00
Mohand Abdelkader <mohand27m@gmail.com>
2025-04-12 12:17:03 +0200

    adding Applayout and header and loader and featch data

--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -1,5 +1,20 @@
+import { getMenu } from "../../service/apiRestaurant";
+import MenuItem from "./MenuItem";
+import { useLoaderData } from "react-router-dom";
 function Menu() {
-  return <h1>Menu</h1>;
+  const menu = useLoaderData();
+
+  return (
+    <ul>
+      {menu.map((pizza) => (
+        <MenuItem pizza={pizza} key={pizza.id} />
+      ))}
+    </ul>
+  );
 }
 
+export async function loader() {
+  const menu = await getMenu();
+  return menu;
+}
 export default Menu;
---
+++ src/ui/Header.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <header>
+      <Link to="/">Fast React Pizza</Link>
+      <p>Mohand</p>
+    </header>
+  );
+}
+
+export default Header;
